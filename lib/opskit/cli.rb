2a94b4acd007238fdee4cb060311d9c8c982019a
MarkArts <markarts@hoppinger.com>
2016-08-19 16:23:46 +0200

    Added color coding

--- lib/opskit/cli.rb
+++ lib/opskit/cli.rb
@@ -10,16 +10,17 @@ module OpsKit
     desc "setup", "Setup a project based on a git repo"
     def setup (repo, name=nil)
       if !name
-        name = repo.split('/').last.split(".").first
+        name = repo.split('/').last.split(".git").first
       end
 
       OpsKit.configure name
 
-      if Dir.exists? name
+      if Dir.exists? OpsKit.configuration.project_root
         return if no? "This project already exists do you want to overwrite it?"
         clean name
       end
 
+      say "Cloning repo into #{OpsKit.configuration.project_root}", :cyan
       run "git clone #{repo} #{name}"
 
       if yes? "create vhost?"
@@ -27,9 +28,14 @@ module OpsKit
         ask_for_docroot
 
         vhost = OpsKit::VHost.new( {template: OpsKit.configuration.template, url: OpsKit.configuration.url, docroot: OpsKit.configuration.docroot } )
+        say "Create vhost for #{vhost.conf[:url]} at #{vhost.vhost_location}", :cyan
         run "echo '#{vhost.render}' | sudo tee #{vhost.vhost_location}"
         run "sudo a2ensite #{vhost.conf[:url]}"
+
+        say "Creating hosts entry", :cyan
         run "grep -q -F '127.0.0.1 #{vhost.conf[:url]}' /etc/hosts || echo '127.0.0.1 #{vhost.conf[:url]}' | sudo tee -a /etc/hosts"
+
+        say "Reload the apache2 server", :cyan
         run "sudo service apache2 reload"
       end
 
@@ -39,26 +45,32 @@ module OpsKit
     def clean (name)
       OpsKit.configure name
 
-      if Dir.exists? name
+      if Dir.exists? OpsKit.configuration.project_root
         return if no? "This will remove #{OpsKit.configuration.project_root} are you sure?"
         FileUtils.rm_rf(OpsKit.configuration.project_root)
       else
         return if no? "Can´t find the project continue cleaning?"
       end
 
-
       if yes? "clean vhost?"
 
         ask_for_url
         vhost = OpsKit::VHost.new( {template: OpsKit.configuration.template, url: OpsKit.configuration.url, docroot: OpsKit.configuration.docroot } )
 
+        say "Removing the hosts entry for #{vhost.conf[:url]}", :cyan
         run "sed '/127.0.0.1 #{vhost.conf[:url]}/d' /etc/hosts | sudo tee /etc/hosts"
+
+        say "Disabling #{vhost.conf[:url]}", :cyan
         run "sudo a2dissite #{vhost.conf[:url]}"
 
+        say "Remove #{vhost.vhost_location}", :cyan
         if File.exists? vhost.vhost_location
           run "sudo rm #{vhost.vhost_location}"
+        else
+          say "Couldn't find #{vhost.vhost_location}", :red
         end
 
+        say "Reload the apache2 server", :cyan
         run "sudo service apache2 reload"
       end
     end
